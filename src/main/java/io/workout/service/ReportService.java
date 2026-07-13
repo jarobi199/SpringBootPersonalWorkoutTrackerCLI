7f3f82335e50ceda09c3b3458402e4e27ead3402
ventrya <dpryce@ventrya.com>
2026-07-13 02:52:58 -0400

    Another commit.

--- src/main/java/io/workout/service/ReportService.java
+++ src/main/java/io/workout/service/ReportService.java
@@ -30,45 +30,49 @@ public class ReportService {
             List<WorkoutSession> workoutSessions = workoutSessionRepository.findByUserIdOrderBySessionDateTimeDesc(SessionContext.getUser().getId());
             for (WorkoutSession workoutSession : workoutSessions) {
                 List<SessionEntry> sessionEntries = workoutSession.getSessionEntries().stream()
-                        .filter(sessionEntry ->  sessionEntry.exerciseId().equals(exercise.getId())).toList();
-                        if(exerciseListMap.containsKey(exercise))
-                        {
-                            exerciseListMap.get(exercise).addAll(sessionEntries);
-                        }
-                        else
-                        {
-                            exerciseListMap.put(exercise, sessionEntries);
-                        }
+                        .filter(sessionEntry ->  sessionEntry.exerciseId().equals(exercise.getId())).collect(Collectors.toCollection(ArrayList::new));
+                if(!sessionEntries.isEmpty()) {
+                    if(exerciseListMap.containsKey(exercise))
+                    {
+                        exerciseListMap.get(exercise).addAll(sessionEntries);
+                    }
+                    else
+                    {
+                        exerciseListMap.put(exercise, sessionEntries);
+                    }
+                }
             }
         }
 
-        System.out.println();
-        System.out.println("| TOP EXERCISES |");
-        Table topExercisesTable = Clique.table(TableType.BOX_DRAW)
-                .headers(
-                        "[*blue, bold]EXERCISE NAME[/]",
-                        "[*blue, bold]EXERCISE TYPE[/]",
-                        "[*blue, bold]TOTAL ENTRIES[/]",
-                        "[*blue, bold]TOTAL VOLUME[/]",
-                        "[*blue, bold]AVERAGE VOLUME PER ENTRY[/]"
-                );
+        if(!exerciseListMap.isEmpty()){
+            System.out.println();
+            System.out.println("| TOP EXERCISES |");
+            Table topExercisesTable = Clique.table(TableType.BOX_DRAW)
+                    .headers(
+                            "[*blue, bold]EXERCISE NAME[/]",
+                            "[*blue, bold]EXERCISE TYPE[/]",
+                            "[*blue, bold]TOTAL ENTRIES[/]",
+                            "[*blue, bold]TOTAL VOLUME[/]",
+                            "[*blue, bold]AVERAGE VOLUME PER ENTRY[/]"
+                    );
 
-        Map<Exercise, List<SessionEntry>> exerciseListMapSorted = exerciseListMap.entrySet()
-                .stream()
-                .sorted(Comparator.comparingInt(entry -> entry.getValue().size()))
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue,
-                        LinkedHashMap::new
-                ));
+            Map<Exercise, List<SessionEntry>> exerciseListMapSorted = exerciseListMap.entrySet()
+                    .stream()
+                    .sorted(Comparator.comparingInt(entry -> entry.getValue().size()))
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (oldValue, newValue) -> oldValue,
+                            LinkedHashMap::new
+                    )).reversed();
 
-        for (Map.Entry<Exercise, List<SessionEntry>> entry : exerciseListMapSorted.entrySet()) {
-            int totalEntries = entry.getValue().size();
-            int totalVolume = entry.getValue().stream().mapToInt(e -> entry.getKey().calculateVolume(e)).sum();
-            double averageVolume =  entry.getValue().stream().mapToInt(e -> entry.getKey().calculateVolume(e)).average().getAsDouble();
-            topExercisesTable.row(entry.getKey().getName(), entry.getKey().getExerciseType().name(), String.valueOf(totalEntries), String.valueOf(totalVolume), String.valueOf(averageVolume));
+            for (Map.Entry<Exercise, List<SessionEntry>> entry : exerciseListMapSorted.entrySet()) {
+                int totalEntries = entry.getValue().size();
+                int totalVolume = entry.getValue().stream().mapToInt(e -> entry.getKey().calculateVolume(e)).sum();
+                double averageVolume =  entry.getValue().stream().mapToInt(e -> entry.getKey().calculateVolume(e)).average().getAsDouble();
+                topExercisesTable.row(entry.getKey().getName(), entry.getKey().getExerciseType().name(), String.valueOf(totalEntries), String.valueOf(totalVolume), String.valueOf(averageVolume));
+            }
+            topExercisesTable.render();
         }
-        topExercisesTable.render();
     }
 }
